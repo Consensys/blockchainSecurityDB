a012a16708fdec965e08834ec4a1e1b70f1dcd64
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-06-04 12:18:50 -0700

    Create script to generate project pages

---
+++ utils/jsonToMd.js
@@ -0,0 +1,153 @@
+const projects = require('./projects.json');
+
+function convertJsonToMd(indexes) {
+  // Months map
+  const monthsMap = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December',
+    '1': 'January',
+    '2': 'February',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+  }
+
+  // Final value variables
+  let title;
+  let projectURL;
+  let description;
+  let audits = [];
+  audits.auditTitles = [];
+  audits.auditURLs = [];
+  audits.auditDates = [];
+  audits.auditors = [];
+  audits.efforts = [];
+  audits.repos = [];
+  audits.commitHashes = [];
+  let bountyURL;
+  let bountyMax;
+  let securityContact;
+
+  // Intermediary array variables
+  let titleArr = [];
+  let projectURLArr = [];
+  let descriptionArr = [];
+  let auditTitlesArr = [];
+  let auditURLsArr = [];
+  let auditDatesArr = [];
+  let auditorsArr = [];
+  let effortsArr = [];
+  let reposArr = [];
+  let commitHashesArr = [];
+  let bountyURLArr = [];
+  let bountyMaxArr = [];
+  let securityContactArr = [];
+
+  // Get intermediary array values
+  indexes.some((index, i) => {
+    if(projects[index]["Listed"] !== false) {
+      titleArr[i] = projects[index]["Project"];
+      projectURLArr[i] = projects[index]["Project URL"];
+      descriptionArr[i] = projects[index]["Description"];
+      auditTitlesArr[i] = projects[index]["Audit Title"];
+      auditURLsArr[i] = projects[index]["Audit URL"];
+      auditDatesArr[i] = projects[index]["Date of Audit"];
+      auditorsArr[i] = projects[index]["Auditor"];
+      effortsArr[i] = projects[index]["Person-Weeks Effort"];
+      reposArr[i] = projects[index]["Repo"];
+      commitHashesArr[i] = projects[index]["Commit Hash"];
+      bountyURLArr[i] = projects[index]["Bounty URL"];
+      bountyMaxArr[i] = projects[index]["Bounty Max Payout"];
+      securityContactArr[i] = projects[index]["Security Contact Email"];
+    }
+  });
+
+  // Get final values
+  for(i = 0; i < indexes.length; i++) {
+    titleArr[i] ? title = titleArr[i] : null;
+    projectURLArr[i] ? projectURL = projectURLArr[i] : null;
+    descriptionArr[i] ? description = descriptionArr[i] : null;
+    auditTitlesArr[i] ? audits.auditTitles.push(auditTitlesArr[i]) : null;
+    auditURLsArr[i] ? audits.auditURLs.push(auditURLsArr[i]) : null;
+    // Format dates
+    if(auditDatesArr[i]) {
+      const date = auditDatesArr[i];
+      const month = monthsMap[date.split('/')[0]];
+      const year = `20${date.split('/')[1]}`;
+      const dateString = `${month}, ${year}`;
+      audits.auditDates.push(dateString);
+    }
+    auditorsArr[i] ? audits.auditors.push(auditorsArr[i]) : null;
+    effortsArr[i] ? audits.efforts.push(effortsArr[i]) : null;
+    reposArr[i] ? audits.repos.push(reposArr[i]) : null;
+    commitHashesArr[i] ? audits.commitHashes.push(commitHashesArr[i]) : null;
+    bountyURLArr[i] ? bountyURL = bountyURLArr[i] : null;
+    bountyMaxArr[i] ? bountyMax = bountyMaxArr[i] : null;
+    securityContactArr[i] ? securityContact = securityContactArr[i] : null;
+  }
+
+  let auditMdArr = [];
+
+  for(i = 0; i < audits.auditTitles.length; i++) {
+    if(audits.auditTitles[i]) {
+      auditMdArr[i] = 
+      `#### [${audits.auditTitles[i]}](${audits.auditURLs[i]})
+
+        ${audits.auditDates[i]}<br>
+        Audited by: ${audits.auditors[i]}<br>
+        Effort: ${audits.efforts[i]}<br>
+        ${audits.repos[i] ? `[Repo](${audits.repos[i]})` : ''}
+      `
+    }
+  }
+
+  const auditMd = `
+    ## Audits
+
+    ${Array(auditMdArr.length).join(0).split(0).map((item, i) => `
+      ${auditMdArr[i]}
+    `).join('')}
+  `
+
+  const fullMd = `
+    # ${title}
+
+    [${projectURL}](${projectURL})<br>
+    ${description}
+
+    ${auditMd}
+
+    ${bountyURL ? 
+    `## Bounty
+    
+    [${bountyURL}](${bountyURL})<br>
+    Max payout: ${bountyMax}
+    ` : ''}
+    
+    ${securityContact ? 
+    `## Additional Info
+    Security Contact: ${securityContact}
+    ` : ''}
+  `
+
+  console.log(fullMd);
+}
+
+convertJsonToMd([4, 5, 6, 7, 8, 9, 10]);
