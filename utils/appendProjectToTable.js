81520175e0ade1fd0b1d9f2f907503d53bf731d6
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-06-10 16:40:30 -0700

    Create script to parse json project files to markdown table row

---
+++ utils/appendProjectToTable.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const fs = require('fs');
+
+function appendProjectToTable(file) {
+  const jsonFile = require(`../projects/${file}`);
+  const fileName = file.split('.')[0];
+
+  // Months map
+  const monthsMap = {
+    '01': 'Jan.',
+    '02': 'Feb.',
+    '03': 'Mar.',
+    '04': 'Apr.',
+    '05': 'May.',
+    '06': 'Jun.',
+    '07': 'Jul.',
+    '08': 'Aug.',
+    '09': 'Sep.',
+    '10': 'Oct.',
+    '11': 'Nov.',
+    '12': 'Dec.',
+    '1': 'Jan.',
+    '2': 'Feb.',
+    '3': 'Mar.',
+    '4': 'Apr.',
+    '5': 'May.',
+    '6': 'Jun.',
+    '7': 'Jul.',
+    '8': 'Aug.',
+    '9': 'Sep.',
+    '10': 'Oct.',
+    '11': 'Nov.',
+    '12': 'Dec.'
+  }
+
+  // Get individual audits
+  let auditMdArr = [];
+  for(i = 0; i < jsonFile.audits.length; i++) {
+    const audit = jsonFile.audits[i];
+    // Format date
+    let auditDate;
+    if(audit.date.length > 0) {
+      const month = monthsMap[audit.date.split('/')[0]];
+      const year = `20${audit.date.split('/')[1]}`;
+      auditDate = `${month} ${year}`;
+    }
+
+    auditMdArr[i] = `
+<li>[${audit.title} (${audit.auditor}, ${auditDate})](${audit.url})</li>
+    `
+  }
+
+  // Join audits
+  const auditMd = `
+<ul>
+${Array(auditMdArr.length).join(0).split(0).map((item, i) => `
+${auditMdArr[i] !== undefined ? auditMdArr[i] : ''}
+`).join('')}
+</ul>
+  `
+
+  const tableMd = `
+|[${jsonFile.project}](projects/${fileName}.md)|${jsonFile.description}|${auditMd}|${jsonFile.bounty ? jsonFile.bounty : ''}|${jsonFile.bounty ? jsonFile.bounty_max : ''}|${jsonFile.security_contact ? jsonFile.security_contact : ''}|
+  `
+
+  const formattedTableMd = tableMd.split('\n').join('');
+
+  const writePath = path.join(__dirname, '../docs');
+  fs.appendFile(`${writePath}/index.md`, `\n${formattedTableMd}`, (err) => {
+    if(err) throw err;
+    console.log('Table row successfully appended.');
+  })
+}
+
+const projectsPath = path.join(__dirname, '../projects');
+fs.readdir(projectsPath, (err, files) => {
+  files.forEach(file => {
+    appendProjectToTable(file);
+  })
+});
