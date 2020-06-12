39b1fce0603faabc0d58c9fc1fba7fcf253f4b72
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-06-12 15:22:32 -0700

    Add basic disclaimer

--- utils/createProjectTable.js
+++ utils/createProjectTable.js
@@ -79,6 +79,8 @@ function createProjectTable() {
 
 The Blockchain Security DB is an open-source database created by ConsenSys Diligence for all things blockchain security. The database contains a catalog of blockchain projects with details pertaining to their security including audits, bounties, and security contacts. Click on the name of the project in the project column to see more details about a project. To add or update a project, see [this guide](https://github.com/ConsenSys/blockchainSecurityDB#add-or-update-a-project).
 
+This project simply serves as an aggregation of blockchain security data, and does not guarantee the security of any particular project. 
+
 |Project|Audits|Bounty Program|Max Bounty|
 |-------|------|--------------|----------|`
 
